73f325b9112349ff9413ca967e32bcf876d8965f
Hariharandurairangasamy <harishyuvi2124@gmail.com>
2023-03-09 21:53:42 +0530

    Auth route added

--- src/Controller/Users/UsersRoute.ts
+++ src/Controller/Users/UsersRoute.ts
@@ -1,16 +1,18 @@
 import { Router } from "express";
 import UsersController from "./Users";
+import AuthMiddleware from "../../Middleware/auth"
 
 class UsersRouotes{
     router:Router
     public UsersController :UsersController = new UsersController()
+    public authMiddleware :AuthMiddleware = new AuthMiddleware()
     constructor(){
         this.router = Router()
         this.init()
     }
     init():void{
-        this.router.post("/postUserData",this.UsersController.PostUsers)
-        this.router.get("/getUsers",this.UsersController.getUserData)
+        this.router.post("/postUserData",this.authMiddleware.routeAuthMiddleware,this.UsersController.PostUsers)
+        this.router.get("/getUsers",this.authMiddleware.routeAuthMiddleware,this.UsersController.getUserData)
         this.router.delete("/deleteUsers/:id",this.UsersController.deleteuserData)
         this.router.post("/userLogin",this.UsersController.userLogin)
     }
